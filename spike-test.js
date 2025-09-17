14f3f4cf6a13a735e5d383614cac423736c61ab5
iavorkata1 <iavorkata1@gmail.com>
2025-09-17 20:39:39 +0300

    first commit

---
+++ spike-test.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+vus: 20,
+
+    stages: [
+        {
+            duration: '30s',
+            target: 100,
+        },
+         {
+            duration: '30s',
+            target: 0,
+        }
+    ]
+}; 
+
+export default function () {
+    http.get('https://quickpizza.grafana.com/test.k6.io/');
+    sleep(1);
+}
+
---
+++ stress-test.js
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+vus: 20,
+
+    stages: [
+        {
+            duration: '15s',
+            target: 10,
+        },
+         {
+            duration: '1m',
+            target: 10,
+        },
+         {
+            duration: '15s',
+            target: 0,
+        }
+    ]
+}; 
+
+export default function () {
+    http.get('https://quickpizza.grafana.com/test.k6.io/');
+    sleep(1);
+    http.get('https://quickpizza.grafana.com/contacts.php');
+    sleep(2);
+    http.get('https://quickpizza.grafana.com/news.php');
+    sleep(3);
+}
+
